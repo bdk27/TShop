2c0fa3d2ec9844eb9826c7f498688d60476c308e
BDK27 <briansam195@gmail.com>
2022-09-04 13:11:44 +0800

    nav,hero-RWD

---
+++ script.js
@@ -0,0 +1,19 @@
+let menu = document.getElementById('menu');
+let navbar = document.getElementById('navbar');
+
+
+menu.onclick = function(){
+    navbar.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
